4d4936b606143ccea8d323224eb4c77e3712089c
SujnKim <blackout3210@naver.com>
2022-03-08 10:45:56 +0000

    upload 5-9.py

--- DFSBFS/5-9.py
+++ DFSBFS/5-9.py
@@ -6,7 +6,7 @@ from collections import deque
 # BFS 메서드 정의
 def bfs(graph, start, visited):
 
-    # 큐 구현을 위해 deque 라이브러리 사용
+    # 큐 구현을 위해 deque 라이브러리 사용함
     queue = deque([start])
     # 현재 노드 방문 처리
     visited[start] = True
