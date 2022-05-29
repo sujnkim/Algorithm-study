9dc7508881eed51044a425d9db606634d214d24d
SujnKim <blackout3210@naver.com>
2022-05-29 08:42:00 +0000

    Modify 11-4.py

--- resolve/11-4.py
+++ resolve/11-4.py
@@ -14,13 +14,14 @@ target = 1
 
 for d in data:
 
+    # 현재 1 ~ target-1까지의 모든 금액을 만들 수 있다고 가정
     # target보다 단위가 클 경우, target은 만들 수 없음
     if target < d:
         break
 
     # target보다 단위가 작거나 같을 경우
     # 이전의 결과와 d를 조합하여 만들 수 있음
-    # target을 더 큰 수로 변경(갱신)
+    # 다음에 확인할 수로 target을 변경(갱신)
     target += d
 
 print(target)
